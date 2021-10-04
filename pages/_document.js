37dcb6eade6679bc14bc1751392be8b99051ef28
amory <pnv170691@gmail.com>
2021-10-04 21:31:52 +0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -50,8 +50,9 @@ export default class MyDocument extends Document {
             {/* we optimize this later */}
             <link
                 rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                href="https://fonts.googleapis.com/css?family=Montserrat"
             />
+            <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
           </Head>
           <body>
           <Main />
